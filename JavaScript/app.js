ad98542e58d42ce87742785cfdd74a1ba52a9bdc
MuradBayramovDeveloper <125645966+MuradBayramov1@users.noreply.github.com>
2023-03-30 19:13:20 +0400

    I created a playlist and added its functions

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -10,33 +10,36 @@ const currentTime = document.querySelector('#current-time')
 const progressBar = document.querySelector('#progress-bar')
 const volume = document.querySelector('#volume')
 const volumeBar = document.querySelector('#volume-bar')
+const ul = document.querySelector('ul')
 
 
 const player = new MusicPlayer(musicList);
 
-window.addEventListener('load', () =>  {
+window.addEventListener('load', () => {
     let music = player.getMusic();
     displayMusic(music)
+    displayMusicList(player.musicList)
+    isPlayingNow()
 })
 
-function displayMusic(music){
+function displayMusic(music) {
     title.innerText = music.getName();
     image.src = "img/" + music.img;
     audio.src = "mp3/" + music.file;
 }
 
-play.addEventListener('click',  () =>  {
+play.addEventListener('click', () => {
     audio.play()
     const isMusicPlay = container.classList.contains('playing')
     isMusicPlay ? pauseMusic() : playMusic();
 })
 
-function pauseMusic(){
+function pauseMusic() {
     container.classList.remove('playing');
     audio.pause();
     play.classList = "fa-solid fa-play";
 }
-function playMusic(){
+function playMusic() {
     container.classList.add('playing');
     play.classList = "fa-solid fa-pause";
     audio.play();
@@ -49,20 +52,22 @@ next.addEventListener('click', () => {
     nextMusic();
 })
 
-function prevMusic(){
+function prevMusic() {
     player.prev();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow()
 }
-function nextMusic(){
+function nextMusic() {
     player.next();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
+    isPlayingNow()
 }
 
-function calculateTime(allSeconds){
+function calculateTime(allSeconds) {
     const minute = Math.floor(allSeconds / 60);
     const second = Math.floor(allSeconds % 60);
     const uptadeSecond = second < 10 ? `0${second}` : `${second}`;
@@ -72,8 +77,8 @@ function calculateTime(allSeconds){
 }
 
 audio.addEventListener('loadedmetadata', () => {
-     duration.textContent = calculateTime(audio.duration);
-     progressBar.max = Math.floor(audio.duration)
+    duration.textContent = calculateTime(audio.duration);
+    progressBar.max = Math.floor(audio.duration)
 });
 
 audio.addEventListener('timeupdate', () => {
@@ -91,12 +96,12 @@ let soundStatus = "voice";
 volumeBar.addEventListener('input', (e) => {
     const value = e.target.value;
     audio.volume = value / 100;
-    if(value == 0){
+    if (value == 0) {
         audio.muted = true;
         soundStatus = 'muted';
         volume.classList = "fa-solid fa-volume-xmark"
         volumeBar.value = 0;
-    }else{
+    } else {
         audio.muted = false;
         soundStatus = "voice"
         volume.classList = "fa-solid fa-volume-high"
@@ -104,13 +109,13 @@ volumeBar.addEventListener('input', (e) => {
 })
 
 volume.addEventListener('click', () => {
-    if(soundStatus === "voice"){
+    if (soundStatus === "voice") {
         audio.muted = true;
         soundStatus = 'muted';
         volume.classList = "fa-solid fa-volume-xmark"
         volumeBar.value = 0;
-      
-    }else{
+
+    } else {
         audio.muted = false;
         soundStatus = "voice"
         volume.classList = "fa-solid fa-volume-high"
@@ -118,7 +123,49 @@ volume.addEventListener('click', () => {
     }
 })
 
-if(volumeBar.value === 0){
+if (volumeBar.value === 0) {
     volume.classList = "fa-solid fa-volume-xmark"
-   
+
+}
+
+function displayMusicList(list) {
+    for (let i=0; i < list.length; i++) {
+        let liTag = `
+        <li onClick="selectedMusic(this)" li-index='${i}' class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${list[i].getName()}</span>
+        <span id="music-${i}" class="badge bg-primary rounded-pill"></span>
+        <audio class="music-${i}" src="mp3/${list[i].file}"></audio>
+        </li>
+        `;
+
+        ul.insertAdjacentHTML("beforeend", liTag);
+
+        let liAudioDuration = ul.querySelector(`#music-${i}`);
+        let liAudioTag = ul.querySelector(`.music-${i}`);
+
+        liAudioTag.addEventListener("loadeddata", () => {
+            liAudioDuration.innerText = calculateTime(liAudioTag.duration)
+        }) ;
+
+        
+    }
+} 
+
+function selectedMusic(li){
+    player.index = li.getAttribute("li-index");
+    displayMusic(player.getMusic());
+    playMusic();
+    isPlayingNow()
+}
+
+function isPlayingNow(){
+    for(let li of ul.querySelectorAll("li")){
+        if(li.classList.contains("playing")){
+            li.classList.remove("playing")
+            
+        }
+        if(li.getAttribute("li-index") == player.index){
+            li.classList.add("playing")
+        }
+    }
 }
