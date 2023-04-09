43c5a2ea6cd4217e2288e683199abb51ce254256
Murad Bayramov <110521210+bayramovmurad@users.noreply.github.com>
2023-04-09 12:02:22 +0400

    Music text update

--- JavaScript/music.js
+++ JavaScript/music.js
@@ -13,6 +13,6 @@ class Music {
 
 const musicList = [
     new Music("Night Lovell", "Live Television", "1.png","1.mp3"),
-    new Music("Night Lovell", "Dark Night", "3.png","2.mp3"),
-    new Music("Host", "Deira City Centre", "2.png","3.mp3")
+    new Music("Night Lovell", "Dark Light", "3.png","2.mp3"),
+    new Music("Night Lovell", "Deira City Centre", "2.png","3.mp3")
 ];
